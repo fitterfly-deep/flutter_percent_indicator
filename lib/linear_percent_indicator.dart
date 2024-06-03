c3007fea1c6750178ea96cbfb8dd86a164cbedb9
fitterfly-deep <132646851+fitterfly-deep@users.noreply.github.com>
2024-06-03 17:57:36 +0530

    Update linear_percent_indicator.dart

--- lib/linear_percent_indicator.dart
+++ lib/linear_percent_indicator.dart
@@ -282,7 +282,7 @@ class _LinearPercentIndicatorState extends State<LinearPercentIndicator>
             Positioned(
               right: widget.isRTL ? percentPositionedHorizontal : null,
               left: !widget.isRTL ? percentPositionedHorizontal : null,
-              top: _containerHeight / 2 - _indicatorHeight,
+              top: _containerHeight / 2 - _indicatorHeight - 1.5,
               child: widget.widgetIndicator!,
             ),
         ],
